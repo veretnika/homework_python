b9aa0b98854edce89e1a56cc4ff329a786e15600
veretnika <89999019+veretnika@users.noreply.github.com>
2021-10-19 14:43:51 +0300

    Add files via upload

---
+++ 06-10/06-10.py
@@ -0,0 +1,27 @@
+print("Введите имя")
+spravochnik_tel = {}
+
+def spravochnik(spravochnik_tel):
+    while True:
+        name = str(input())
+        if name != 'q':
+            print("Введите номер телефона c +7-***-***--")
+            a = str(input())
+            if a[0] == "+" and a[1] == "7" and a[2] == "-" and \
+                a[6] == "-" and a[10] == "-" and a[13] == "-" and len(a) == 16 :
+                if a[1:].replace("-", "").isdigit():
+                    spravochnik_tel[name] = a
+                    print("Введите имя")
+                else:
+                    print("Неправильный формат телефона, попробуйте снова")
+                    print("Введите имя")
+            else:
+                    print("Неправильный формат телефона, попробуйте снова")
+                    print("Введите имя")
+        if name == 'q':
+            print("Ваш справочник")
+            print(spravochnik_tel)
+            break
+
+
+spravochnik(spravochnik_tel)
